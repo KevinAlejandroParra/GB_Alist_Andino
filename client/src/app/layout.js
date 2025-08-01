a9750f0ae2283ff233bdbaabca57c3234e50447f
Kevin Alejandro Parra Cifuentes <luisparra5380@gmail.com>
2025-08-01 18:50:12 +0000

    build:(client)test login refactor

--- client/src/app/layout.js
+++ client/src/app/layout.js
@@ -1,3 +1,5 @@
+import "./globals.css"
+
 export const metadata = {
   title: 'ALIST - Control y Mantenimiento de Máquinas',
   description: 'Sistema de gestión para el control y mantenimiento de máquinas de la sede Andino',
