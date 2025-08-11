bf37a362e7993612c5e02958243f06953574ae9f
Alejandro <luisparra5380@gmail.com>
2025-08-11 13:32:25 -0500

    feat:(client) adds HeroAndNavbar component and updates the structure of the main page

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,19 +1,13 @@
 "use client"; 
-
 import Link from 'next/link';
 import { AuthProvider } from '../components/AuthContext';
+import HeroAndNavbar from '../components/home/HeroAndNavbar';
 import "./globals.css";
 
 export default function Home() {
   return (
     <AuthProvider>
-      <div className="container">
-        <h1>Bienvenido a ALIST</h1>
-        <div className="auth-links">
-          <Link href="/login" className="auth-link">Iniciar Sesión</Link>
-          <Link href="/register" className="auth-link">Registrarse</Link>
-        </div>
-      </div>
+<HeroAndNavbar />
     </AuthProvider>
   );
 }
