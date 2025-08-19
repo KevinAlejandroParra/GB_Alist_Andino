6af48575f9ef17464e268ad2975260c90bb34eee
Kevin Alejandro Parra Cifuentes <luisparra5380@gmail.com>
2025-08-19 18:12:46 +0000

    config(server):access control allow of cors

--- server/src/index.js
+++ server/src/index.js
@@ -1,20 +1,46 @@
-    const express = require('express');
-    const app = express();
-    require('dotenv').config();
-    const port = 5000;
-    const userRoutes = require("./routes/userRoutes");
-    const swaggerUi = require('swagger-ui-express');
-    const swaggerFile = require('./swagger-output.json');
-    app.use(express.json()); 
-    const cors = require('cors');
-    app.use(cors({
-        origin: '*',
-        credentials: true,
-    }));
+const express = require('express');
+const app = express();
+require('dotenv').config();
+const port = 5000;
+const userRoutes = require("./routes/userRoutes");
+const swaggerUi = require('swagger-ui-express');
+const swaggerFile = require('./swagger-output.json');
+const path = require("path"); 
+app.use(express.json()); 
+const cors = require('cors');
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
-    app.use("/api/users", userRoutes);
-    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
+app.use(cors({
+  origin: "*",
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+}));
 
-  app.listen(port, '0.0.0.0', () => {
+app.use(express.static(path.join(__dirname, "../public"), { 
+   setHeaders: (res, path) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+  
+    
+    // Headers adicionales críticos
+    res.setHeader("Access-Control-Allow-Methods", "GET");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    
+    // Header para tipos MIME 
+    if (path.endsWith(".png")) {
+      res.setHeader("Content-Type", "image/png");
+    }
+    if (path.endsWith(".jpg") || path.endsWith(".jpeg")) {
+      res.setHeader("Content-Type", "image/jpeg");
+    }
+  }
+}));
+
+
+app.use("/api/users", userRoutes);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
+app.listen(port, '0.0.0.0', () => {
   console.log(`Servidor escuchando en http://0.0.0.0:${port}`);
 });
