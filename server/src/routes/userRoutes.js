4bbe8f045eda8ef53d3e3975f9be38b8d2250d71
Kevin Alejandro Parra Cifuentes <luisparra5380@gmail.com>
2025-08-19 18:08:37 +0000

    feat(server):naming and addressing of files

--- server/src/routes/userRoutes.js
+++ server/src/routes/userRoutes.js
@@ -2,16 +2,36 @@ const express = require('express');
 const UserController = require('../controllers/userController');
 const { verifyToken } = require('../middleware/authMiddleware');
 const { checkRole } = require('../middleware/authMiddleware');
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
 
 const router = express.Router();
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    const folder = path.join(__dirname, "..", "..", "public", "images", "users"); 
+    cb(null, folder);
+  },
+  filename: function (req, file, cb) {
+    const ext = path.extname(file.originalname);
+    const fileName = `user-${req.params.user_id}-${Date.now()}${ext}`;
+    cb(null, fileName);
+  },
+});
 
+const fileFilter = (req, file, cb) => {
+  const allowed = /jpeg|jpg|png|gif/;
+  const isValid = allowed.test(file.mimetype);
+  isValid ? cb(null, true) : cb(new Error("Tipo de archivo no permitido"), false);
+};
+
+const upload = multer({ storage, fileFilter });
 
 router.get('/', verifyToken, checkRole([1]), (req, res, next) => {
-    console.log('Ruta de obtener usuarios llamada'); // Imprimir mensaje al acceder a la ruta
     next();
 }, UserController.getUsers); // Obtener todos los usuarios
 router.post('/', UserController.createUser); // Crear un usuario
-router.put('/:user_id', UserController.updateUser); // Actualizar un usuario
+router.put('/:user_id', upload.single("imagen"),UserController.updateUser); // Actualizar un usuario
 router.delete('/:user_id', UserController.deleteUser); // Eliminar un usuario definitivamente
 router.patch('/:user_id/state', UserController.changeUserState); // Cambiar el estado de un usuario temporalmente
 router.post('/login', UserController.loginUser); // Ruta para iniciar sesión
