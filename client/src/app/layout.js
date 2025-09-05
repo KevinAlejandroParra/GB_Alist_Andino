bfbc8f926a7c37f711d93837373feb3e6de812fe
Alejandro <luisparra5380@gmail.com>
2025-09-05 13:44:45 -0500

    feat(client): Adds the authentication context to the main layout, wrapping the children in the authentication provider to manage the user state in the application.

--- client/src/app/layout.js
+++ client/src/app/layout.js
@@ -1,4 +1,5 @@
-import "./globals.css"
+import "./globals.css";
+import { AuthProvider } from '../components/AuthContext';
 
 export const metadata = {
   title: 'ALIST - Control y Mantenimiento de Máquinas',
@@ -8,7 +9,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
-      <body>{children}</body>
+      <body>
+        <AuthProvider>
+          {children}
+        </AuthProvider>
+      </body>
     </html>
   );
 }
