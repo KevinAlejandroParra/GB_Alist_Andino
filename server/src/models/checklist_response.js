7016915061f71646f10e41419fb15fdc267c5230
Alejandro <luisparra5380@gmail.com>
2025-09-09 15:59:40 -0500

    feat(server): The association method is added to the ChecklistResponse model to establish relationships with ChecklistItem and Checklist, and the value type is updated to ENUM to improve response management.

--- server/src/models/checklist_response.js
+++ server/src/models/checklist_response.js
@@ -1,55 +1,61 @@
-"use strict";
-const { Model } = require("sequelize");
+'use strict';
+const { Model } = require('sequelize');
 
 module.exports = (sequelize, DataTypes) => {
-    class ChecklistResponse extends Model {}
-    ChecklistResponse.init(
-        {
-            response_id: {
-                type: DataTypes.INTEGER,
-                autoIncrement: true,
-                primaryKey: true,
-            },
-            checklist_id: {
-                type: DataTypes.INTEGER,
-                allowNull: false,
-            },
-            checklist_item_id: {
-                type: DataTypes.INTEGER,
-                allowNull: false,
-            },
-            value: {
-                type: DataTypes.BOOLEAN,
-                allowNull: true, // Permitir que el valor sea nulo inicialmente
-            },
-            comment: {
-                type: DataTypes.STRING,
-            },
-            evidence_url: {
-                type: DataTypes.STRING,
-            },
-            responded_by: {
-                type: DataTypes.INTEGER,
-                allowNull: false,
-            },
-            createdAt: {
-                type: DataTypes.DATE,
-                defaultValue: DataTypes.NOW,
-                allowNull: false,
-            },
-            updatedAt: {
-                type: DataTypes.DATE,
-                defaultValue: DataTypes.NOW,
-                onUpdate: DataTypes.NOW,
-                allowNull: false,
-            },
-        },
-        {
-            sequelize,
-            modelName: "ChecklistResponse",
-            timestamps: true,
-            tableName: "checklist_responses",
-        }
-    );
-    return ChecklistResponse;
+  class ChecklistResponse extends Model {
+    static associate(models) {
+      // Una respuesta pertenece a un elemento específico de la lista de comprobación.
+      ChecklistResponse.belongsTo(models.ChecklistItem, {
+        foreignKey: 'checklist_item_id',
+        as: 'item',
+      });
+      // Una respuesta forma parte de una instancia de lista de comprobación mayor.
+      ChecklistResponse.belongsTo(models.Checklist, {
+        foreignKey: 'checklist_id',
+      });
+      // Una respuesta puede tener un fallo asociado si el valor es “no cumple” o tiene un comentario.
+      ChecklistResponse.hasOne(models.Failure, {
+        foreignKey: 'response_id',
+        as: 'failure',
+      });
+    }
+  }
+  ChecklistResponse.init(
+    {
+      response_id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      checklist_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      checklist_item_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      value: {
+        type: DataTypes.ENUM('cumple', 'no cumple', 'observación'),
+        allowNull: true, // Inicialmente nulo hasta que se envíe una respuesta
+      },
+      comment: {
+        type: DataTypes.STRING,
+      },
+      evidence_url: {
+        type: DataTypes.STRING,
+      },
+      responded_by: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+    },
+    {
+      sequelize,
+      modelName: 'ChecklistResponse',
+      tableName: 'checklist_responses',
+      timestamps: true,
+    }
+  );
+  return ChecklistResponse;
 };
